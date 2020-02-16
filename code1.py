b6ea689f03a489307c090b106a03a12e88e303d5
FerretEsq <tomtziv@gmail.com>
2020-02-16 13:03:38 +0000

    modified:   code1.py

--- code1.py
+++ code1.py
@@ -6,8 +6,9 @@ import os, fnmatch, zipfile, time, subprocess
 start=time.time()
 
 basepath='/home/ferret/CL5235_k1828612_Toms/' # Change directory to 'User' later
-logsdir='CL5235_Logs'
+logsdir='CL5235_Logs/' # Logs directory
 bsdst=basepath+logsdir # Base destination to work with
+script=basepath+'evtx_dump.py'
 
 # Attempts to make logs directory if it doesnt exist. works fine for now
 try:
@@ -15,8 +16,8 @@ try:
 except FileExistsError:
     print('Folder exists!')
 
-# Reads directory and prints out all items for now.  
-# Printed with respectable folder names and a whitespace.
+
+# Extracts all directories and files into newly created (or existing) 
 with zipfile.ZipFile(basepath+'/evtx_logs.zip') as zipref:
     zipref.extractall(bsdst) # Extracts evtx_log.zip into logs directory to work with
 
@@ -24,7 +25,6 @@ directories=os.scandir(bsdst+'/evtx_logs') # Directories to work with
 
 counter1=0 # Subdirectory counter
 counter2=0 # File counter
-
 for directory in directories:
     print('Working on {0} directory'.format(directory.name))
     counter1+=1 # Prints the name of the directory and increments counter
@@ -33,15 +33,17 @@ for directory in directories:
             #if item.is_file():
             if fnmatch.fnmatch(item,'*.xml'):
                 print('You have an XML file, please remove it.') # Tests for XML file extensions
+                break
             counter2+=1
             print('Converting {0}...'.format(item.name)) # Prints name of file and increments counter 
-            subprocess.call(['python','evtx_dump.py',item.name])
+            #subprocess.call(['python3','evtx_dump.py',item.name])
+            # python3 ~/CL5235_k1828612_Toms/evtx_dump.py PanacheSysmon_vs_AtomicRedTeam01.evtx > pan.xml
+            #os.system(command='python3 {0} {1}evtx_logs/{2} > {2} '.format(script,bsdst,item.name))
             print('Conversion complete!')
     print() # Prints whitespace for readability
 
-elapsed=time.time()-start
 
-print('Program finished in {0:.2f} seconds'.format(elapsed))
+print('Program finished in {0:.2f} seconds'.format(time.time()-start))
 print('Traversed {0} folders'.format(counter1))
 print('Converted {0} files'.format(counter2))
         
